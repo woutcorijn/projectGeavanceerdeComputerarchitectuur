e33ecd4319c5419b9cd422ea297dbb4b9470c066
christoph20035 <46644563+christoph20035@users.noreply.github.com>
2025-03-17 19:25:33 +0100

    fix ray length calculations

--- render.cu
+++ render.cu
@@ -67,8 +67,7 @@ __global__ void calculateLengthRays(Ray *rays, Circle *circlesObject, Circle sou
     Ray &ray = rays[index];
 
     // y = helling * x + a
-    double helling = sin(ray.angle)/cos(ray.angle);
-    double a = (ray.y+source.y) - helling * (ray.x+source.x);
+    
     //printf("x: %d, y: %d", ray.x, ray.y);
 
     // (x - c.x)^2 + (y - c.y)^2 = r^2 -> circle
@@ -89,20 +88,58 @@ __global__ void calculateLengthRays(Ray *rays, Circle *circlesObject, Circle sou
     // Diskriminant = B^2 - 4*A*C
     // x1 = (-B + sqrt(B^2 - 4*A*C)) / (2*A)
     // x2 = (-B - sqrt(B^2 - 4*A*C)) / (2*A)
+    double rayDirX = cos(ray.angle);
+    double rayDirY = sin(ray.angle);
 
+    double helling = rayDirY/rayDirX;
+    double a = (ray.y+source.y) - helling * (ray.x+source.x);
+    
     double A = 1.0 + helling*helling;
+
+    bool lengthSet = false;
     for(Circle circle: circles){
         double B = 2.0 * helling * (a - circle.y) - 2.0 * circle.x;
         double C = circle.x*circle.x + (a - circle.y)*(a - circle.y) - circle.radius_square;
-
         double discriminant = B*B - 4.0*A*C;
+
         if (discriminant >= 0.0) {
             double x1 = (-B + sqrt(discriminant)) / (2.0*A);
             double x2 = (-B - sqrt(discriminant)) / (2.0*A);
-            ray.length = (int)(x1 < x2 ? x1 : x2);
+            // bereken y op circle
+            double y1 = helling * x1 + a;
+            double y2 = helling * x2 + a;
+
+            // scalair product om te kijken of de circle in de richting van de ray valt
+            double vec1X = x1 - (ray.x + source.x);
+            double vec1Y = y1 - (ray.y + source.y);
+            double dot1 = rayDirX * vec1X + rayDirY * vec1Y;
+            
+            double length1 = sqrt((x1 - ray.x-source.x) * (x1 - ray.x-source.x) + (y1 - ray.y-source.y) * (y1 - ray.y-source.y));
+            double length2 = sqrt((x2 - ray.x-source.x) * (x2 - ray.x-source.x) + (y2 - ray.y-source.y) * (y2 - ray.y-source.y));
+            if(dot1 > 0){
+                if (length1 < length2) {
+                    if(lengthSet == true && length1 < ray.length || lengthSet == false){
+                        lengthSet = true;
+                        ray.length = (int)length1;
+                    }
+                } else {
+                    if(lengthSet == true && length2 < ray.length || lengthSet == false){
+                        lengthSet = true;
+                        ray.length = (int)length2;
+                    }
+                }
+                if(ray.length > WIDTH+HEIGHT){
+                    ray.length = WIDTH+HEIGHT;
+                } 
+            }
+            else{
+                ray.length = WIDTH+HEIGHT;
+            }
         }
         else{
-            ray.length = WIDTH+HEIGHT;
+            if(lengthSet == false){
+                ray.length = WIDTH+HEIGHT;
+            }
         }
 
     }
